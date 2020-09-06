00f02fd0fa45d91898ecb6150d13c2dcb91a6cf8
IrynaMysiuk <iruna.musyk8a@gmail.com>
2020-09-06 21:30:29 +0300

    Fix fpr MainGmailBO

--- src/main/java/com/epam/lab/business_objects/MainGmailBO.java
+++ src/main/java/com/epam/lab/business_objects/MainGmailBO.java
@@ -2,18 +2,16 @@ package com.epam.lab.business_objects;
 
 import com.epam.lab.page_objects.MainGmailPO;
 
-import static com.epam.lab.singleton.DriverManager.getDriver;
+
+import static com.epam.lab.singleton.DriverContainer.getDriver;
 import static com.epam.lab.utils.Constants.GMAIL;
 
 public class MainGmailBO {
-    protected MainGmailPO mainGmailPO;
     private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(MainGmailBO.class);
 
-    public MainGmailBO() {
-        mainGmailPO = new MainGmailPO();
-    }
-
     public void openGmailPage() {
+        MainGmailPO mainGmailPO = new MainGmailPO();
+        mainGmailPO.waitGoogleLogo();
         log.info("Check loading Gmail page");
         mainGmailPO.typeText(GMAIL);
         log.info("Page title is:" + getDriver().getTitle());
@@ -22,6 +20,7 @@ public class MainGmailBO {
     }
 
     public void logIn() {
+        MainGmailPO mainGmailPO = new MainGmailPO();
         log.info("Log in");
         mainGmailPO.logIn();
     }
